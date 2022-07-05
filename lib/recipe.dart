70d6b3a46a87ac315a24deb6937d064a562bd437
Suyatna <zuyatna@gmail.com>
2022-07-05 20:35:53 +0700

    adding slider that show how many serves and the quantity goes increase/decrease

--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,18 +1,78 @@
 class Recipe {
   String label;
   String imageUrl;
-  // TODO: Add serving and ingredient here
+  int serving;
+  List<Ingredient> ingredients;
 
-  Recipe(this.label, this.imageUrl);
+
+  Recipe(this.label, this.imageUrl, this.serving, this.ingredients);
 
   static List<Recipe> samples = [
-    Recipe('Spaghetti and Meatballs', 'assets/2126711929_ef763de2b3_w.jpg'),
-    Recipe('Tomato Soup', 'assets/27729023535_a57606c1be.jpg'),
-    Recipe('Grilled Cheese', 'assets/3187380632_5056654a19_b.jpg'),
-    Recipe('Chocolate Chip Cookies', 'assets/15992102771_b92f4cc00a_b.jpg'),
-    Recipe('Taco Salad', 'assets/8533381643_a31a99e8a6_c.jpg'),
-    Recipe('Hawaiian Pizza', 'assets/15452035777_294cefced5_c.jpg'),
+    Recipe(
+      'Spaghetti and Meatballs',
+      'assets/2126711929_ef763de2b3_w.jpg',
+      4,
+      [
+        Ingredient(1, 'box', 'spaghetti'),
+        Ingredient(4, '', 'frozen Meatball'),
+        Ingredient(0.5, 'jar', 'sauce'),
+      ],
+    ),
+    Recipe(
+      'Tomato Soup',
+      'assets/27729023535_a57606c1be.jpg',
+      2,
+      [
+        Ingredient(1, 'can', 'tomato Soup'),
+      ],
+    ),
+    Recipe(
+      'Grilled Cheese',
+      'assets/3187380632_5056654a19_b.jpg',
+      1,
+      [
+        Ingredient(2, 'slices', 'cheese'),
+        Ingredient(2, 'slices', 'bread'),
+      ],
+    ),
+    Recipe(
+      'Chocolate Chip Cookies',
+      'assets/15992102771_b92f4cc00a_b.jpg',
+      24,
+      [
+        Ingredient(4, 'cups', 'flour'),
+        Ingredient(2, 'cups', 'sugar'),
+        Ingredient(0.5, 'cups', 'chocolate chips'),
+      ],
+    ),
+    Recipe(
+      'Taco Salad',
+      'assets/8533381643_a31a99e8a6_c.jpg',
+      1,
+      [
+        Ingredient(4, 'oz', 'nachos'),
+        Ingredient(3, 'oz', 'taco meat'),
+        Ingredient(0.5, 'cup', 'cheese'),
+        Ingredient(0.25, 'cup', 'chopped tomatoes'),
+      ],
+    ),
+    Recipe(
+      'Hawaiian Pizza',
+      'assets/15452035777_294cefced5_c.jpg',
+      4,
+      [
+        Ingredient(1, 'item', 'pizza'),
+        Ingredient(1, 'cup', 'pineapple'),
+        Ingredient(8, 'oz', 'ham'),
+      ],
+    ),
   ];
 }
 
-// TODO: Add Ingredient() here
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(this.quantity, this.measure, this.name);
+}
--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -11,7 +11,7 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
-  // TODO: Add _sliderVal here
+  int _sliderVal = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -34,8 +34,32 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
             ),
-            // TODO: Add Expanded
-            // TODO: Add Slider() here
+            Expanded(
+                child: ListView.builder(
+                    padding: const EdgeInsets.all(7.0),
+                    itemCount: widget.recipe.ingredients.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final ingredient = widget.recipe.ingredients[index];
+                      return Text(
+                          '${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient.name}'
+                      );
+                    }
+                )
+            ),
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 10,
+              label: '${_sliderVal * widget.recipe.serving} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
